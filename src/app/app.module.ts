b368b745cad2ceecea56ed26b4ff1dbc9d87e33d
Wwb <285562253@qq.com>
2018-03-26 14:40:13 +0800

    测试数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,56 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
+import { RestProvider } from '../providers/rest/rest';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { DiscoveryPage } from '../pages/discovery/discovery';
+import { ChatPage } from '../pages/chat/chat';
+import { NotificationPage } from '../pages/notification/notification';
+import { MorePage } from '../pages/more/more';
+import { LoginPage } from '../pages/login/login';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IonicStorageModule } from '@ionic/storage'
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
+    TabsPage,
+    LoginPage,
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    RestProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
